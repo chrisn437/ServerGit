8ba0317ec6c755aaa845249173424febf95301b6
chrisn437 <92748142+chrisn437@users.noreply.github.com>
2021-11-16 15:04:27 +0100

    oscNetwork message reciever and sender

--- src/project/oscNetwork.py
+++ src/project/oscNetwork.py
@@ -1,5 +1,16 @@
-from src.project.parser import Parser
+import argparse
+import math
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+
 from src.Structs.Constants import SAVED_SCENE
+from src.project.parser import Parser
+
+
 class OscNetwork():
     """ OSC Message receiver and sender
     """
@@ -7,3 +18,20 @@ class OscNetwork():
     def __init__(self):
         self.sceneParser = Parser(SAVED_SCENE)
         print("Initializing OSC interface")
+
+        ip = "127.0.0.1"
+        sendPort = 7000
+        inPort = 8000
+
+        #Sending osc message
+        client = udp_client.SimpleUDPClient(ip, sendPort)
+
+        #catch OSC message
+        dispater = dispatcher.Dispatcher()
+        dispater.map()
+
+        #Serer for listening
+        server = osc_server.ThreadingOSCUDPServer((ip, inPort),dispatcher)
+        print("Servering on {}".format(server.server_address))
+        server.serve_forever()
+
